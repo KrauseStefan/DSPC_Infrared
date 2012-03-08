98f632853b87273f5e6833744e833d9112bf718c
Stefan Krause-Kjaer <stefan.krausekjaer@gmail.com>
2012-03-08 13:51:58 +0100

    Added Counter TestBench file.

--- QuartusPrj/Counter.vhd
+++ QuartusPrj/Counter.vhd
@@ -1,10 +1,41 @@
-
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity Counter is
-	port (
-		clk : in std_logic;
-		rst : in std_logic
+	port(
+		clk   : in  std_logic;          -- 1,512 MHz
+		IR_RX : in  std_logic;
+		sr_in : in std_logic;
+		shift : in std_logic;
+		data  : out std_logic := '0';
+		valid : out std_logic := '0'
 	);
 end entity Counter;
+
+architecture Counter_Arc of Counter is
+	variable rcvCount : integer := 0;
+	signal bitShiftReg : std_logic_vector(7 downto 0) := x"00";
+	signal intValid : std_logic := 0;
+begin
+	receive : process(clk) is
+	begin
+		if rising_edge(clk) then 
+			bitShiftReg(7 downto 1) <= bitShiftReg(6 downto 0);
+			bitShiftReg(0) <= IR_RX;
+
+			case bitShiftReg is
+				when x"ff" =>
+					valid <= '1';
+					data <= '1';
+				when x"00" =>
+					valid <= '1';
+					data <= '0';					
+				when others => 
+					valid <= '0';			
+			end case;				
+
+		end if;
+	end process receive;
+
+end architecture Counter_Arc;
+
